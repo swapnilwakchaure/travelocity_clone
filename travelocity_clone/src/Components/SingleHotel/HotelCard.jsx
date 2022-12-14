f2b1370070e8828f01751fad43dcafdf92a271e1
Aman Dwivedi <amandwivedi71124.com>
2022-12-14 20:33:28 +0530

    Fw20_0455-Day-2

---
+++ travelocity_clone/src/Components/SingleHotel/HotelCard.jsx
@@ -0,0 +1,38 @@
+import { Box,Flex,Spacer,Text,Button } from '@chakra-ui/react'
+import { ArrowBackIcon } from '@chakra-ui/icons'
+import {AiOutlineHeart} from "react-icons/ai"
+import React from 'react'
+import "./HotelCard.css"
+import Tabss from './Tabss'
+const HotelCard = () => {
+  return (
+    <Box border="1px solid black" h="30rem" w="80%" m="auto" pt={5}>
+      <Flex w="98%" m="auto" h={45} >
+      <ArrowBackIcon/>
+        <Text ml={2} color="blue.400">See All Properties</Text>
+        <Spacer/>
+        <AiOutlineHeart />
+        <Text ml={2} color="blue.400">Save</Text>
+      </Flex>
+      <Flex>
+        <img id='Bimage' src="https://images.trvl-media.com/lodging/6000000/5310000/5301700/5301691/796fe3c1.jpg?impolicy=resizecrop&rw=598&ra=fit" alt="" height="2rem" />
+        <div className='G1'>
+        <img id='gimages' src="https://images.trvl-media.com/lodging/6000000/5310000/5301700/5301691/f27b2428.jpg?impolicy=resizecrop&rw=297&ra=fit" alt="" />
+        <img id='gimages' src="https://images.trvl-media.com/lodging/6000000/5310000/5301700/5301691/f27b2428.jpg?impolicy=resizecrop&rw=297&ra=fit" alt="" />   
+        <img id='gimages' src="https://images.trvl-media.com/lodging/6000000/5310000/5301700/5301691/f27b2428.jpg?impolicy=resizecrop&rw=297&ra=fit" alt="" />
+        <img id='gimages' src="https://images.trvl-media.com/lodging/6000000/5310000/5301700/5301691/f27b2428.jpg?impolicy=resizecrop&rw=297&ra=fit" alt="" />
+      </div>
+      </Flex>
+      <Flex>
+      <Tabss/>
+      <Spacer />
+      <Button color="white" colorScheme="blue" bg="blue" mr={5}>
+        Reserve a room
+      </Button>
+      </Flex>
+      
+    </Box>
+  )
+}
+
+export default HotelCard
---
+++ travelocity_clone/src/Components/SingleHotel/Overview.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const overview = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default overview
