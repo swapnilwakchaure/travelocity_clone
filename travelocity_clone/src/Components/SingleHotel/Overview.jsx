126c2310f8a05baf06e52876562034db5add1208
Aman Dwivedi <amandwivedi71124.com>
2022-12-15 22:50:17 +0530

    added day-3 work

--- travelocity_clone/src/Components/SingleHotel/Overview.jsx
+++ travelocity_clone/src/Components/SingleHotel/Overview.jsx
@@ -1,11 +1,111 @@
+import { Box, Flex, Spacer, Text } from '@chakra-ui/react'
 import React from 'react'
-
-const overview = () => {
+import {BiSwim} from "react-icons/bi"
+import {FaCanadianMapleLeaf,FaSnowflake} from "react-icons/fa"
+import {SlPaypal} from "react-icons/sl"
+import {MdLocalLaundryService} from "react-icons/md"
+import {AiFillStar,AiOutlineRight,AiOutlineWifi,AiOutlineLeft} from "react-icons/ai"
+const Overview = () => {
   return (
-    <div>
-      
-    </div>
+    <Flex>
+      <Box>
+          {/* Left side */}
+          <Text fontSize="4xl" fontWeight={'bold'} align='left'>
+            St.Regis Mumbai
+            </Text>
+           
+            <Flex >
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            <AiFillStar/>
+            </Flex>
+            <br />
+          <Text fontSize={'2xl'} fontWeight="bold" align={'left'}>
+          9.2/10 Wonderful
+          </Text>
+          <Flex>
+          <Text color={'blue.500'} align={'left'}>
+            See All Views </Text>
+            <Box color={'blue.500'} mt={1.5} ml={2}>
+            <AiOutlineRight/>
+            </Box>
+          </Flex>
+          <br />
+          <Text fontSize={'xl'} fontWeight="bold" align={'left'}>
+          Popular amenities
+          </Text>
+          <Flex  w={500} h='auto' color={'gray.700'} pt={5}>
+                <Flex w={'50%'} h={8} align='left'>
+                <AiOutlineWifi/>
+                <Text ml={2}>Wifi</Text>
+                </Flex>
+                <Spacer />
+                <Flex w={'50%'} h={8}  align='left'>
+
+                <BiSwim/>
+                <Text ml={2}>Pool</Text>
+                </Flex>
+          </Flex>
+          <Flex  w={500} h='auto' color={'gray.700'} pt={5}>
+                <Flex w={'50%'} h={8} align='left'>
+                <FaCanadianMapleLeaf/>
+                <Text ml={2}>Spa</Text>
+                </Flex>
+                <Spacer />
+                <Flex w={'50%'} h={8} align='left'>
+
+                <MdLocalLaundryService/>
+                <Text ml={2}>Laundry</Text>
+                </Flex>
+          </Flex>
+          <Flex  w={500} h='auto' color={'gray.700'} pt={5}>
+                <Flex w={'50%'} h={8} align='left'>
+                <FaSnowflake/>
+                <Text ml={2}>Air conditioning
+                </Text>
+                </Flex>
+                <Spacer />
+                <Flex w={'50%'} h={8}  align='left'>
+
+                <SlPaypal/>
+                <Text ml={2}>Parking included
+
+                </Text>
+                </Flex>
+          </Flex>
+          <Flex color="blue.500">
+            <Box mt={1.5} mr={2}>
+            <AiOutlineLeft/>
+            </Box>
+            
+            <Text>See All</Text>
+          </Flex>
+      </Box>
+      <Box border={'1px solid black'} w='80%' align='right'>
+        {/* Right Side */}
+        <Box border={'1px solid blue'} h={200} w={'60%'} borderRadius={10} m={4}>
+             {/* map component */}
+             Map
+        </Box>
+        <Box w={350}>
+        <Text>
+       462 Senapati Bapat Marg, Lower Parel, Mumbai, Maharashtra, 400013
+       </Text>
+       <Flex ml={5}>
+       <Text color={'blue.500'} >
+             View in map </Text>
+            <Box color={'blue.500'} mt={1.5} ml={2}>
+            <AiOutlineRight/>
+            
+            </Box>
+            </Flex>
+        </Box>
+
+      </Box>
+    </Flex>
   )
 }
 
-export default overview
+export default Overview
---
+++ travelocity_clone/src/Components/SingleHotel/Rooms.jsx
@@ -0,0 +1,99 @@
+import { Box, Button, Flex, Input, Text } from '@chakra-ui/react'
+import React from 'react'
+import "./Rooms.css"
+import room1 from "../Assets/room1.webp"
+import room2 from "../Assets/room2.webp"
+import room3 from "../Assets/room3.webp"
+import room4 from "../Assets/room4.webp"
+import room5 from "../Assets/room5.webp"
+import room6 from "../Assets/room6.webp"
+import room7 from "../Assets/room7.webp"
+import room8 from "../Assets/room8.webp"
+import room9 from "../Assets/room9.webp"
+import room10 from "../Assets/room10.webp"
+import room11 from "../Assets/room11.webp"
+import RoomsDet from './RoomsDet'
+const Rooms = () => {
+   
+  return (
+    <Box>
+      <Text fontSize={'4xl'} fontWeight="bold" align={'left'}>
+      Choose your room
+      </Text>
+      <Flex gap={2}>
+      <Input borderColor={'black'} focusBorderColor='blue.500' w={200} borderRadius={8}
+ placeholder="gvjv"
+ size="md"
+ type="datetime-local"
+/>
+
+<Input borderColor={'black'} focusBorderColor='blue.500' w={200} borderRadius={8}
+ placeholder="gvjv"
+ size="md"
+ type="datetime-local"
+/>
+
+<Input borderColor={'black'} focusBorderColor='blue.500' w={300} borderRadius={8}
+ placeholder="Travelers"
+ _placeholder={{color:"Gray" ,fontSize:"20px"}}
+ size="md"
+ 
+/>
+<Button  color="white" w={'30%'}
+        colorScheme=""
+        bg="blue.500" >
+    Check Availability
+</Button>
+      </Flex>
+      <div id='Rcards'>
+            <div>
+                <img src={room1} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room2} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room3} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room4} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room5} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room6} alt="" />
+                <RoomsDet/>
+            </div>
+            
+            <div>
+                <img src={room7} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room8} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room9} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room10} alt="" />
+                <RoomsDet/>
+            </div>
+            <div>
+                <img src={room11} alt="" />
+                <RoomsDet/>
+            </div>
+      </div>
+    </Box>
+  )
+}
+
+export default Rooms
