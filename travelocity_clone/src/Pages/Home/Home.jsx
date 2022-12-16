2882ca437c101d42c2e557ff01fc751259257fbc
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2022-12-16 22:59:27 +0530

    day4_work1

---
+++ travelocity_clone/src/Pages/Home/Home.jsx
@@ -0,0 +1,13 @@
+import Footer from "../../Components/FooterPage/FooterPage";
+import HomeForm from "../../Components/HomeForm/HomeForm"
+
+const Home = () => {
+    return (
+        <div>
+            <HomeForm />
+            <Footer />
+        </div>
+    )
+}
+
+export default Home;
