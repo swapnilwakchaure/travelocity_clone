31b14cf13b2df30236465c4314932e9357d4bbe3
Swapnil Wakchaure <swapnilwakchaure2411999@gmail.com>
2022-12-19 12:08:08 +0530

    test3

--- travelocity_clone/src/server.js
+++ travelocity_clone/src/server.js
@@ -2,7 +2,7 @@ const jsonServer = require("json-server"); // importing json-server library
 const server = jsonServer.create();
 const router = jsonServer.router("db.json");
 const middlewares = jsonServer.defaults();
-const port = process.env.PORT || 3001; // you can use any port number here; i chose to use 3001
+const port = process.env.PORT || 8080; // you can use any port number here; i chose to use 3001
 
 server.use(middlewares);
 server.use(router);
